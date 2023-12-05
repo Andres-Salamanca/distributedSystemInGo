d19f4206917f4613d9ee3762ec5511870f798651
Andres-Salamanca <andrealebarbaritos@gmail.com>
2023-12-04 19:58:27 -0500

    ya se hizo el mini get y post de logs

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"log"
+	"github.com/Andres-Salamanca/proglog/internal/server"
+)
+
+
+func main(){
+
+	srv := server.NewHTTPServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Andres-Salamanca/proglog
+
+go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
